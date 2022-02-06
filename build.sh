2a2dedb2bd26c5bde53c339ee5ba7999e898b7e2
tpoulsen93 <t-poulsen@hotmail.com>
2022-02-06 15:21:24 -0700

    updated build.sh to include front end

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-./gradlew build || echo "Build failed"; exit -1
+./gradlew build || (echo "Gradle build failed"; exit 1)
+echo "######################## Gradle Complete ############################" 
+
+npm --prefix ./client install || (echo "npm install failed"; exit 1)
+echo "######################## npm Install Complete #######################" 
+
+npm --prefix ./client run build || (echo "npm run build failed"; exit 1)
+echo "######################## npm Build Complete #########################" 
+
