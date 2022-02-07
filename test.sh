4b9ffb4f80f7498d2dce3a1bedae663255784428
aidanleuck <aidan@leucks.com>
2022-02-07 15:58:48 -0700

    Updated test script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./gradlew test || { echo "NUnit tests on server failed"; exit 1; }
-./gradlew pmdMain || { echo "There are static issues with the main code!"; }
+./gradlew pmdMain || { echo "There are static issues with the main code!";  exit 1;}
 ./gradlew pmdTest || { echo "There are static issues with the test code!";  exit 1; }
-./gradlew checkstyleMain || { echo "Style issues in main code"; exit 1; }
-./gradlew checkstyleTest || { echo "Style issues in test code"; exit 1; }
+./gradlew checkstyleMain || { echo "Style issues in main code"; }
+./gradlew checkstyleTest || { echo "Style issues in test code"; }
