04dff89fef4b0d9a2a77e8f9674e39e0488e3bb0
aidanleuck <aidan@leucks.com>
2022-02-07 15:47:49 -0700

    Updated build/test scripts

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-./gradlew build || (echo "Gradle build failed"; exit 1)
+(./gradlew assemble) || { echo >&2 "Gradle build failed"; exit 1;}
 echo "######################## Gradle Complete ############################" 
 
-npm --prefix ./client install || (echo "npm install failed"; exit 1)
+npm --prefix ./client install || { echo >&2 "npm install failed"; exit 1;}
 echo "######################## npm Install Complete #######################" 
 
-npm --prefix ./client run build || (echo "npm run build failed"; exit 1)
+npm --prefix ./client run build || { echo >&2 "npm run build failed"; exit 1; }
 echo "######################## npm Build Complete #########################" 
 
--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-exit 127
+./gradlew test || { echo "NUnit tests on server failed"; exit 1; }
+./gradlew pmdMain || { echo "There are static issues with the main code!"; }
+./gradlew pmdTest || { echo "There are static issues with the test code!";  exit 1; }
+./gradlew checkstyleMain || { echo "Style issues in main code"; exit 1; }
+./gradlew checkstyleTest || { echo "Style issues in test code"; exit 1; }
