5cd63034424a4eaa49c5c794c04a852198f2c271
keeganhanesboise <keeganhanes@u.boisestate.edu>
2022-02-14 20:57:48 -0700

    Link linter to test script

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+npm --prefix ./client run lint
+
 ./gradlew test || { echo "NUnit tests on server failed"; exit 1; }
 ./gradlew pmdMain || { echo "There are static issues with the main code!";  exit 1;}
 ./gradlew pmdTest || { echo "There are static issues with the test code!";  exit 1; }
