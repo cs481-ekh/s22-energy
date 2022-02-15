84054fa720cc66cc2ae7016c895c65c53a74cd29
tpoulsen <50130117+tpoulsen93@users.noreply.github.com>
2022-02-14 20:40:42 -0700

    Closes #4 (#21)

--- build.sh
+++ build.sh
@@ -1,9 +1,36 @@
-(./gradlew assemble) || { echo >&2 "Gradle build failed"; exit 1;}
-echo "######################## Gradle Complete ############################" 
+#!/bin/bash
 
-npm --prefix ./client install || { echo >&2 "npm install failed"; exit 1;}
-echo "######################## npm Install Complete #######################" 
+# print a horizontal rule
+rule() {
+  printf -v _hr "%*s" $(tput cols) && echo "${_hr// /${1--}}"
+}
 
-npm --prefix ./client run build || { echo >&2 "npm run build failed"; exit 1; }
-echo "######################## npm Build Complete #########################" 
+# print a horizontal ruler with mesage
+rulem() {
+  if [ $# -eq 0 ]; then
+    echo "Usage: rulem <message> [<rule character>]"
+    return 1
+  fi
 
+  # fill line with ruler characters ($2, default "-"), reset cursor, move 2 cols right, print message
+  printf -v _hr "%*s" $(tput cols) &&
+    echo "" &&
+    echo -en "${_hr// /${2--}}" &&
+    echo -e "\r\033[2C$1" &&
+    echo ""
+}
+
+
+
+rule '='
+rulem "[ gradle build ]" '='
+rule '='
+(./gradlew assemble) || { rulem "Gradle build failed" '!'; exit 1; }
+rulem "[ gradle build complete ]" '#'
+
+rule '='
+rulem "[ npm build ]" '='
+rule '='
+npm --prefix ./client install || { rulem "npm install failed" '!'; exit 1; }
+npm --prefix ./client run build || { rulem "npm run build failed" '!'; exit 1; }
+rulem "[ npm build complete ]" '#'
--- test.sh
+++ test.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
-./gradlew test || { echo "NUnit tests on server failed"; exit 1; }
-./gradlew pmdMain || { echo "There are static issues with the main code!";  exit 1;}
-./gradlew pmdTest || { echo "There are static issues with the test code!";  exit 1; }
-./gradlew checkstyleMain || { echo "Style issues in main code"; }
-./gradlew checkstyleTest || { echo "Style issues in test code"; }
+
+# print a horizontal rule
+rule() {
+  printf -v _hr "%*s" $(tput cols) && echo "${_hr// /${1--}}"
+}
+
+# print a horizontal ruler with mesage
+rulem() {
+  if [ $# -eq 0 ]; then
+    echo "Usage: rulem <message> [<rule character>]"
+    return 1
+  fi
+
+  # fill line with ruler characters ($2, default "-"), reset cursor, move 2 cols right, print message
+  printf -v _hr "%*s" $(tput cols) &&
+    echo "" &&
+    echo -en "${_hr// /${2--}}" &&
+    echo -e "\r\033[2C$1" &&
+    echo ""
+}
+
+# run gradle tests
+rule '='
+rulem "[ gradle test ]" '='
+rule '='
+./gradlew test || { rulem "NUnit tests on server failed" '!'; exit 1; }
+./gradlew pmdMain || { rulem "There are static issues with the main code!" '!';  exit 1; }
+./gradlew pmdTest || { rulem "There are static issues with the test code!" '!';  exit 1; }
+./gradlew checkstyleMain || { rulem "Style issues in main code" '!'; }
+./gradlew checkstyleTest || { rulem "Style issues in test code" '!'; }
+rulem "[ gradle test complete ]" '#'
+
+# run npm tests
+rule '='
+rulem "[ npm test ]" '='
+rule '='
+npm --prefix ./client install || { rulem "npm install failed" '!'; exit 1; }
+npm --prefix ./client test --silent -- --watchAll=false || { rulem "npm test failed" '!'; exit 1; }
+rulem "[ npm test complete ]" '#'
