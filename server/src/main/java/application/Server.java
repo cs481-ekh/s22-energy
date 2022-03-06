54049e34e48396792b7d968cfcb6584eebcf681d
aidanleuck <aidan@leucks.com>
2022-03-05 20:36:58 -0700

    Added Electric Demand Parser

--- server/src/main/java/application/Server.java
+++ server/src/main/java/application/Server.java
@@ -60,7 +60,7 @@ public class Server implements ApplicationRunner {
     }
     @Override
     public void run(ApplicationArguments arg0) throws Exception {
-        CsvParser parser = new ElectricDemandParser("/home/aidanleuck/Downloads/Data.csv", buildRepo);
+        CsvParser parser = new ElectricDemandParser("/home/aidanleuck/Downloads/Data.csv", 2, buildRepo);
         Response response = parser.readData();
     }
 }
