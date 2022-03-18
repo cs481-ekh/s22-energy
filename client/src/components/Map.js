13aa82de30f59997e64bfd6cd22489036429d7c4
keeganhanesboise <70166916+keeganhanesboise@users.noreply.github.com>
2022-03-17 20:43:34 -0600

    Added usage GET request and removed unused functions

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,58 +1,73 @@
 import React, { Component } from 'react';
+import "../Map.css";
 import PropTypes from 'prop-types';
 import SideDrawer from "./SideDrawer";
 
-// Example array of building data for the function below it
+// Holds building usage data (currently natural gas from 10/31/00 - 03/17/22)
+// Not using the map data structure because of naming conflicts with this component lol
+const usageData = [];
+
+// Holds building data
+//const buildingData = [];
+
+// Temp building data until building data endpoint is implemented
 var buildingData = [
     {
-      lat: 43.603714,
-      long: -116.204826,
-      title: "Administration Building",
-      description: "Uses some energy",
-      size: '150'   // size of circle around building in feet
+        lat: 43.603714,
+        long: -116.204826,
+        title: "Administration Building",
+        description: "Uses some energy",
+        building: '004',
+        size: '150'   // size of circle around building in feet
     },
     {
-      lat: 43.6029000,
-      long: -116.195803,
-      title: "Albertsons Stadium",
-      description: "Uses some energy",
-      size: '300'
+        lat: 43.6029000,
+        long: -116.195803,
+        title: "Albertsons Stadium",
+        description: "Uses some energy",
+        size: '035'
     },
     {
-      lat: 43.614856,
-      long: -116.203084,
-      title: "CCP",
-      description: "Uses some energy",
-      size: '150'
+        lat: 43.614856,
+        long: -116.203084,
+        title: "CCP",
+        description: "Uses some energy",
+        size: '367'
     },
     {
         lat: 43.600704,
         long: -116.19925,
         title: "HEMG",
         description: "Uses some energy",
-        size: '150'
+        size: '005'
     },
     {
         lat: 43.60458,
         long: -116.198939,
         title: "Chaffee Hall",
         description: "Uses some energy",
-        size: '200'
+        size: '031'
     }
 ];
 
-// Create boundary for map (N, W, S, E)
-var bounds;
-function getBounds() {
-    bounds = new window.Microsoft.Maps.LocationRect.fromLocations(
-        new window.Microsoft.Maps.Location(43.787795, -116.658338), 
-        new window.Microsoft.Maps.Location(43.419856, -115.886613)
-    );    
-}
 
-// Adds building from buildingData to the map by creating a location, pin, infobox 
-// and event handler for each building.
-/*
+// GET request that populates usage data array
+const getUsage = async () => {
+    const requestOptions = {
+        method: 'GET',
+        mode: 'cors',
+    };
+    const response = await fetch("http://localhost:5000/usage?start=2000-10-31&end=2022-03-17&utilIID=1", requestOptions);
+    const responseJson = await response.json();
+    responseJson.forEach(building => usageData.push({
+        building: building.buildingCode,
+        utilityUseage: building.utilityUsage
+        // id, utilityID, timestamp, cost
+    }));
+};
+
+// Adds buildings from buildingData to the map by creating a location, pin, infobox
+// and event handler for each building. Infobox displays each building's usage info
 function createPins(map) {
 
     // Create empty array for building pins and info boxes
@@ -84,63 +99,14 @@ function createPins(map) {
         map.entities.push(pinArray[i]);
     }
 }
-*/
 
-// Currently just creates an example polygon for the Extra Mile Arena
-/*
-function createPolygons(map) {
-    var extraMileArenaLocation = new window.Microsoft.Maps.Location(43.603564, -116.198948);
-    var extraMileArena = new window.Microsoft.Maps.Polygon([
-    new window.Microsoft.Maps.Location(43.603964, -116.199576),
-    new window.Microsoft.Maps.Location(43.603964, -116.198326),
-    new window.Microsoft.Maps.Location(43.603160, -116.198326),
-    new window.Microsoft.Maps.Location(43.603160, -116.199576),], { fillColor: 'rgba(0, 0, 255, 0.3', strokeColor: 'orange', strokeThickness: 1 });
-    var extraMileArenaInfo = new window.Microsoft.Maps.Infobox(extraMileArenaLocation, 
-        { 
-            title: 'Extra Mile Arena', 
-            description: 'Uses some energy', 
-            visible: false 
-        }
+// Create boundary for map (N, W, S, E)
+function getBounds() {
+    var bounds = new window.Microsoft.Maps.LocationRect.fromLocations(
+        new window.Microsoft.Maps.Location(43.787795, -116.658338),
+        new window.Microsoft.Maps.Location(43.419856, -115.886613)
     );
-    window.Microsoft.Maps.Events.addHandler(extraMileArena, 'click', function() {
-        extraMileArenaInfo.setOptions({ visible: true });
-    });
-    extraMileArenaInfo.setMap(map);
-    map.entities.push(extraMileArena);
-}
-*/
-
-// Create circular polygon around each building with specified size
-function createCircles(map) {
-    // Create empty array for circles around building and info boxes
-    var infoBoxArray = [];
-    var circleArray = [];
-
-    for (let i = 0; i < buildingData.length; i++) {
-        var building = buildingData[i];
-        var location = new window.Microsoft.Maps.Location(building.lat, building.long);
-        var infoBox = new window.Microsoft.Maps.Infobox(location,
-            {
-                title: building.title,
-                description: building.description,
-                visible: false
-            }  
-        );
-            // Calculate the locations for a regular polygon that has 36 locations which will rssult in an approximate circle.
-            var locs = window.Microsoft.Maps.SpatialMath.getRegularPolygon(location, building.size, 36, window.Microsoft.Maps.SpatialMath.DistanceUnits.Feet);
-            var circle = new window.Microsoft.Maps.Polygon(locs, { fillColor: 'rgba(0, 0, 255, 0.3)', strokeColor: 'orange', strokeThickness: 1 });
-            circleArray.push(circle);
-        infoBoxArray.push(infoBox);
-    }
-
-    // Create event handler for each circle/infobox and add them to the map
-    for (let i = 0; i < circleArray.length; i++) {
-        window.Microsoft.Maps.Events.addHandler(circleArray[i], 'click', function() {
-            infoBoxArray[i].setOptions({ visible: true });
-        });
-        infoBoxArray[i].setMap(map);
-        map.entities.push(circleArray[i]);
-    }
+    return bounds;
 }
 
 class Map extends Component {
@@ -150,10 +116,11 @@ class Map extends Component {
     }
 
     onScriptLoad() {
-        getBounds();
-        const map = new window.Microsoft.Maps.Map(document.getElementById(this.props.id), 
+        getUsage();
+        console.log(usageData);
+        const map = new window.Microsoft.Maps.Map(document.getElementById(this.props.id),
             {
-                maxBounds: bounds,
+                maxBounds: getBounds(),
                 center: new window.Microsoft.Maps.Location(43.605922, -116.202093),
                 customMapStyle: {
                     elements: {
@@ -170,11 +137,7 @@ class Map extends Component {
                 zoom: 15
             }
         );
-        window.Microsoft.Maps.loadModule('Microsoft.Maps.SpatialMath', function() {
-            createCircles(map);
-        });
-        //createPins(map);
-        //createPolygons(map);
+        createPins(map);
     }
 
     componentDidMount() {
@@ -198,12 +161,7 @@ class Map extends Component {
         return (
             <>
                 <SideDrawer/>
-                <div style={{ 
-                    width: "100%",
-                    height: "calc(100VH - 160px)"
-                    }} 
-                    id={this.props.id}>
-                </div>
+                <div id={this.props.id}/>
             </>
         );
     }
