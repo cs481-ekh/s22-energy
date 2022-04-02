32f44e95b6d600a858047268cc33f5b1953563ec
aidanleuck <aidan@leucks.com>
2022-04-02 13:04:19 -0600

    Implemented datepicker logic

--- client/src/components/DatePicker.js
+++ client/src/components/DatePicker.js
@@ -5,38 +5,38 @@ import DatePicker from "@mui/lab/DatePicker";
 import { Stack, TextField, Container } from '@mui/material';
 
 // eslint-disable-next-line react/prop-types
-const DateComponent = ({startDate, setStartDate, endDate, setEndDate}) => {
-  return (
-    <LocalizationProvider dateAdapter={DateFnsAdapter}>
-        <Container maxWidth="sm" sx ={{mt:2}}>
-        <Stack spacing={3}>
-        <DatePicker
-        label="Start Date"
-        value={startDate}
-        onChange={(newValue) =>{
-            setStartDate(newValue);
-        }}
-        renderInput={(params) => <TextField
-            {...params}>
-            </TextField>}
-        
-        />
-        <DatePicker
-        label="End Date"
-        value={endDate}
-        onChange={(newValue) =>{
-            setEndDate(newValue);
-        }}
-        renderInput={(params) => <TextField
-            {...params}>
-            </TextField>}
-        
-        />
-        </Stack>
-        </Container>
-        
-    </LocalizationProvider>
-  );
+const DateComponent = ({ startDate, setStartDate, endDate, setEndDate }) => {
+    return (
+        <LocalizationProvider dateAdapter={DateFnsAdapter}>
+            <Container maxWidth="sm" sx={{ mt: 2 }}>
+                <Stack spacing={3}>
+                    <DatePicker
+                        label="Start Date"
+                        value={startDate}
+                        onChange={(newValue) => {
+                            setStartDate(newValue);
+                        }}
+                        renderInput={(params) => <TextField
+                            {...params}>
+                        </TextField>}
+
+                    />
+                    <DatePicker
+                        label="End Date"
+                        value={endDate}
+                        onChange={(newValue) => {
+                            setEndDate(newValue);
+                        }}
+                        renderInput={(params) => <TextField
+                            {...params}>
+                        </TextField>}
+
+                    />
+                </Stack>
+            </Container>
+
+        </LocalizationProvider>
+    );
 }; 
 
 export default DateComponent;
