cf76dca820d19178c3409036b631d72ab2680b09
aidanleuck <aidan@leucks.com>
2022-04-04 13:49:24 -0600

    Added auth to client side

--- client/src/remote.js
+++ client/src/remote.js
@@ -1,11 +1,23 @@
 const serverPort = process.env.REACT_APP_SERVER_PORT || 5000;
+const userName = process.env.REACT_APP_API_USER;
+const password = process.env.REACT_APP_API_PASSWORD;
+const credentials = `Basic ${Buffer.from(`${userName}:${password}`).toString('base64')}`;
+
+import  { Buffer } from 'buffer';
 let remoteFunctions = {
+  /**
+   * Retrieves list of buildings from server.
+   * @returns list of buildings
+   */
   async getBuildings() {
     let responseJson = {};
     try {
       const requestOptions = {
         method: "GET",
         mode: "cors",
+        headers: {
+          Authorization: credentials
+        },
       };
       const response = await fetch(
         `http://localhost:${serverPort}/building`,
@@ -18,17 +30,30 @@ let remoteFunctions = {
     }
     return responseJson;
   },
+
+  /**
+   * Returns usage from the database
+   * @param {*} startDate - Start date to retrieve usage
+   * @param {*} endDate - End date to retrieve usage
+   * @returns - List of usages
+   */
   async getUsage(startDate, endDate) {
-    startDate = startDate.toISOString().split('T')[0];
-    endDate = endDate.toISOString().split('T')[0];
-    
+    startDate = startDate.toISOString().split("T")[0];
+    endDate = endDate.toISOString().split("T")[0];
+
     let responseJson = {};
     try {
       const requestOptions = {
         method: "GET",
         mode: "cors",
+        headers: {
+          Authorization: credentials
+        },
       };
-      const response = await fetch(`http://localhost:${serverPort}/usage?start=${startDate}&end=${endDate}&utilIID=1`,requestOptions);
+      const response = await fetch(
+        `http://localhost:${serverPort}/usage?start=${startDate}&end=${endDate}&utilIID=1`,
+        requestOptions
+      );
       responseJson = await response.json();
     } catch (err) {
       console.log(err);
