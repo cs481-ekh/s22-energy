69a4ee798f480a18f2501da5f0705795ff76e143
tpoulsen93 <t-poulsen@hotmail.com>
2022-04-04 16:50:45 -0600

    changed startup script

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# build the front end and come back
+# build the back end
+./gradlew build
+
+# build the front end and come back to root
 cd client; npm install; cd ..
 
-# build the back end and come back
-cd server; gradle build; cd ..
-
 # start the frontend and backend together using npm's concurrently
-concurrently "cd server; gradle run --console=plain" "cd client; npm start" 
+concurrently "./gradlew run --console=plain" "cd client; npm start" 
