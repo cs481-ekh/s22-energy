389253da5599f88e0d3e697220d28b21c7d26a04
tpoulsen93 <t-poulsen@hotmail.com>
2022-04-14 19:47:14 -0600

    updated dockerUp.sh

--- dockerUp.sh
+++ dockerUp.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# remove the old database nonsense
 rm -fr postgres-data
+
+# build the docker containers
+docker build -t frontend_and_backend .
+
+# bring the docker containers to life
 docker-compose -f docker-compose.yml up
