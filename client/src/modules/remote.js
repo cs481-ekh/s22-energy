7dac270c01ff8b6198c0dd1731d9c1f7c8fb090e
Eric K. Henderson <ehenderson@boisestate.edu>
2022-04-22 13:05:29 -0600

    [client] fix path to api

--- client/src/modules/remote.js
+++ client/src/modules/remote.js
@@ -1,9 +1,7 @@
-
-const serverPort = process.env.REACT_APP_SERVER_PORT || 5000;
 const userName = process.env.REACT_APP_API_USER;
 const password = process.env.REACT_APP_API_PASSWORD;
 const credentials = `Basic ${Buffer.from(`${userName}:${password}`).toString('base64')}`;
-const baseURL = process.env.REACT_APP_API_URL/energy/api;
+const baseURL = process.env.REACT_APP_API_URL + "/energy/api";
 
 import  { Buffer } from 'buffer';
 let remoteFunctions = {
